945eb32570140533ec285cb302ea9e8d31104ea2
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-10-05 14:40:44 +0100

    list notes

--- spec/notes.js
+++ spec/notes.js
@@ -4,3 +4,17 @@ it('can create a new note', ()=>{
   expect(note.list).toContain ('this is a note')
 });
 
+it('can show a list of notes', ()=>{
+  note = new Note();
+  note.add('this is a note');
+  note.add('this is another note');
+  expect(note.list).toContain ('this is a note', 'this is another note')
+});
+
+xit('can delete a note', ()=>{
+  note = new Note();
+  note.add('this is a note');
+  note.add('this is another note');
+  note.delete('this is another note');
+  expect(note.list).toEqual ('this is a note')
+});
--- src/notes.js
+++ src/notes.js
@@ -6,4 +6,11 @@ class Note {
   add(note){
     this.list.push(note);
   }
+
+  delete(indexInList){
+    // noteIndex = this.list.findIndex(note);
+    // console.log(noteIndex)
+    // this.list.splice(noteIndex, 1);
+    this.list.splice(indexInList, 1)
+  }
 }
