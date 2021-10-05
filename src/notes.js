5c4da16ae3b31f2f6a60ea0955964017694ee5b0
Lilly Daniell <daniell.lilly@gmail.com>
2021-10-05 15:48:24 +0100

    Developing HTML structure of page

--- src/notes.js
+++ src/notes.js
@@ -1,17 +1,31 @@
 class Note {
   constructor(){
     this.list = [];
+    this.abbreviatedList = [];
   }
 
   add(note){
     this.list.push(note);
   }
 
-  delete(note){
+  delete(note){ // update method to complete?
     console.log(this.list)
     let pos = this.list.indexOf(note)
     let removedItem = this.list.splice(pos, 1)
     console.log(this.list)
     return removedItem
   }
+
+  abbreviate(note){
+    let abbreviation = note.substring(0, 20);
+    return abbreviation
+  }
+
+  abbreviateEveryNote() {
+   this.abbreviatedList = this.list.map(note => {
+      this.abbreviate(note) // returns undefined elements in an array
+    });
+  }
+   
+
 }
