a219634edb92c92474bc27de69074913309120a2
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-10-07 01:16:03 +0100

    many pages

--- src/interface.js
+++ src/interface.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const note = new Note(); 
+let note = new Note(); 
 
+// Retrieve (localStorage Object)
+let retrieveNotes = () => {
+  for (let i = 0; i < localStorage.length; i++) {
+    note.list.push(localStorage.getItem(i));
+  }
+}
+
+retrieveNotes();
 
 document.querySelector('#add-note-button').addEventListener('click', (clickevent) => {
   clickevent.preventDefault();
   let newNote = document.querySelector('#add-note').value
-  note.add(newNote); 
   let lastnote = note.lastNotes();    
 
   // This is the API Function
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
-  var raw = JSON.stringify({"text":`${lastnote}`});
+  var raw = JSON.stringify({"text":`${newNote}`});
 
   var requestOptions = {
     method: 'POST',
@@ -27,15 +34,36 @@ document.querySelector('#add-note-button').addEventListener('click', (clickevent
     return response.json()
   })
   .then((data) => {
-    console.log(data.emojified_text)
+    note.add(data.emojified_text)
+    note.abbreviateEveryNote()
 
-    let item = document.createElement('li')
-    let textnode = document.createTextNode(data.emojified_text)  
-    item.appendChild(textnode)  
+    // each note is in an anchor
+    let item = document.createElement('a')
+      item.id = `${note.list.length-1}`; 
+      item.href = `#${note.list.length-1}`; 
+      item.innerText = note.abbreviatedList[note.abbreviatedList.length-1] + '\n'; 
     document.querySelector('#all-notes').appendChild(item) 
-
   })     
+
   
+  makeUrlChangeShowNoteForCurrentPage();
+
+  function makeUrlChangeShowNoteForCurrentPage() {
+    window.addEventListener("hashchange", showNoteForCurrentPage);
+  };
+
+  function showNoteForCurrentPage() {
+    showNote(getNoteFromUrl(window.location));
+  };
+
+  function getNoteFromUrl(location) {
+    return location.hash.split("#")[1];
+  };
+
+  function showNote(Note) {
+    document
+      .getElementById("foo")
+      .innerHTML = note.list[Note];
+  };
   })   
-  console.log(note)
 });
